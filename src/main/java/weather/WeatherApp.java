db55924cce27a8fbc04a4ff00fce40c0a9fb2803
Elsayed Rashed <elsayed.rashed@gmail.com>
2019-06-29 14:15:08 -0400

    First Commit

---
+++ src/main/java/weather/WeatherApp.java
@@ -0,0 +1,13 @@
+package weather;
+
+/**
+ * Hello world!
+ *
+ */
+public class WeatherApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/weather/WeatherAppTest.java
@@ -0,0 +1,20 @@
+package weather;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple WeatherApp.
+ */
+public class WeatherAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
